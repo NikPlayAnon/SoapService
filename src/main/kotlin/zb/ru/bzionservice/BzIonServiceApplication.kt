0cbc8c62f26ef30c0d37cf18886429ced6dc8487
nbogomolov <N_Bogomolov@bz.ru>
2024-04-23 14:15:18 +0300

    Initial commit

---
+++ src/main/kotlin/zb/ru/bzionservice/BzIonServiceApplication.kt
@@ -0,0 +1,11 @@
+package zb.ru.bzionservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BzIonServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BzIonServiceApplication>(*args)
+}
---
+++ src/test/kotlin/zb/ru/bzionservice/BzIonServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package zb.ru.bzionservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BzIonServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
